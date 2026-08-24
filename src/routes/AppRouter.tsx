ce2ff1978d119cd2190c0ce5d3bc3d5f7c991e86
Alessandro-BS <fabrizio.bs9012@gmail.com>
2026-08-23 20:42:53 -0500

    feat (router): Registré ruta para la página de registro

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { ProtectedRoute } from './ProtectedRoute';
 import LoginPage from '../pages/auth/LoginPage';
+import RegisterPage from '../pages/auth/RegisterPage';
 import DashboardPlaceholder from '../pages/DashboardPlaceholder';
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
 
       <Route element={<ProtectedRoute />}>
         <Route path="/dashboard" element={<DashboardPlaceholder />} />
